0c64be98add0ca8d416d1c2fd1b068524da5ca64
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-25 17:06:13 +0545

    Add const file

---
+++ src/const/eng.ts
@@ -0,0 +1,14 @@
+const SurveyStep1Const = {
+  title: 'Thank you for requesting a UXCam quote.',
+  article: 'Just one more thing before you go:',
+  subtitle:
+    'To help us tailor a plan to your needs, please answer the following questions. It will only take 2 minutes!',
+};
+
+const ButtonText = {
+  continue: 'Continue',
+  back: 'Back',
+  submit: 'Submit survey',
+};
+
+export { SurveyStep1Const, ButtonText };
