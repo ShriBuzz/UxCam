128853f71a52605117d42806b9707f4e53c5a0a7
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-25 11:22:57 +0545

    Config colors and font sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-sky': 'var(--primary-sky)',
+        'primary-midnight': 'var(--primary-midnight)',
+        'primary-cloud': 'var(--primary-cloud)',
+        'primary-snow': 'var(--primary-snow)',
+      },
+
+      fontSize: {
+        h1: 'var(--h1)',
+        h2: 'var(--h2)',
+        h3: 'var(--h3)',
+        lg: 'var(--lg)',
+        md: 'var(--md)',
+        sm: 'var(--sm)',
+        xs: 'var(--xs)',
+      },
+    },
   },
   plugins: [],
 };
