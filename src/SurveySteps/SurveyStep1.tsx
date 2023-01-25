d96072641f846fee9fe817e8664237ea2412deb8
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-25 17:06:43 +0545

    Add survey step 1 UI

---
+++ src/SurveySteps/SurveyStep1.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// components
+import Button from '@/components/Button';
+
+// const
+import { SurveyStep1Const, ButtonText } from '@/const/eng';
+
+const SurveyStep1: React.FC = () => {
+  const { title, subtitle, article } = SurveyStep1Const;
+
+  return (
+    <div className='w-full h-full flex-center flex-col text-primary-midnight text-center'>
+      <h1 className='h1 mb-6 max-w-[800px] mt-4'>{title}</h1>
+
+      <p className='roboto'>{article}</p>
+
+      <p className='text-h3 font-semibold text-primary-sky mt-12 mb-16 max-w-[900px]'>
+        {subtitle}
+      </p>
+
+      <Button title={ButtonText.continue} size='large' />
+    </div>
+  );
+};
+
+export default SurveyStep1;
