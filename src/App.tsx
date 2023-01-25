836ce173e19a3c8ea282fd7865c5feb93922c99f
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-25 16:47:22 +0545

    Add generic header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import Logo from '@/components/Logo';
+
 function App() {
   return (
-    <div>
-      <h2 className='text-indigo-500 font-semibold text-center'>Ux Cam</h2>
-    </div>
+    <main className='w-screen h-screen bg-primary-cloud pt-[180px]'>
+      <header className='container h-max flex-center flex-col gap-9 mx-auto'>
+        <Logo />
+
+        <p className='roboto text-sm text-primary-sky tracking-[0.2em] uppercase'>
+          question 1 / 7
+        </p>
+      </header>
+
+      {/* Survey steps container */}
+      <section className='container mx-auto'></section>
+    </main>
   );
 }
 
