f843b216ef682fac5c3498f4183ce0d512f79561
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-25 20:15:16 +0545

    Update AppCardLayout component props

--- src/components/AppCardLayout/index.tsx
+++ src/components/AppCardLayout/index.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 // types
-import {TAppCardLayout} from './types';
+import { TAppCardLayout } from './types';
 
-const AppCardLayout:React.FC<TAppCardLayout> = ({title, image}) => {
+const AppCardLayout: React.FC<TAppCardLayout> = ({
+  title,
+  image,
+  titleSize,
+}) => {
   return (
     <>
-            <span className='w-[120px] h-full flex-center'>
-              <img
-                className='w-[50%] h-[50%] object-contain'
-                src={image}
-              />
-            </span>
+      <span className='w-[120px] lg:w-[100px] xl:w-[120px] h-full flex-center'>
+        {image ? (
+          <img className='w-[50%] h-[50%] object-contain' src={image} />
+        ) : (
+          <span className='w-16 h-16 rounded-full bg-primary-grey' />
+        )}
+      </span>
 
-            <p className='text-h3 font-semibold flex-center'>{title}</p>
-          </>
-  )
-}
+      <p
+        className={`flex-1 justify-start items-center font-semibold flex-center break-all max-w-[90%]
+        ${titleSize === 'medium' ? 'text-2xl' : 'text-h3'}
+      `}
+      >
+        {title}
+      </p>
+    </>
+  );
+};
 
-export default AppCardLayout
+export default AppCardLayout;
