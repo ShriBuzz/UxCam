8cd3162e902f7b590ea0e89d64703b4b23b3d89c
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-26 18:37:32 +0545

    minor fix progress bar styles

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -8,7 +8,7 @@ const ProgressBar: React.FC<TProgressBar> = ({ currentStep, totalSteps }) => {
     <div className='relative w-full h-3 bg-primary-snow'>
       <div
         style={{ width: `${(currentStep / totalSteps) * 100}%` }}
-        className='absolute top-0 left-0 h-full bg-primary-sky transition-all duration-500 ease-in-out'
+        className='absolute max-w-full top-0 left-0 h-full bg-primary-sky transition-all duration-500 ease-in-out'
       ></div>
     </div>
   );
