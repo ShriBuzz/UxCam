0c4d8530487b859b0a66c45f8155798b9fecb6f3
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-26 12:07:34 +0545

    Update main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+
+// components
+import Logo from '@/components/Logo';
+
+import SurveyStep1 from './SurveySteps/SurveyStep1';
+
 function App() {
+  const [step] = useState(0);
+
   return (
-    <div>
-      <h2 className='text-indigo-500 font-semibold text-center'>Ux Cam</h2>
-    </div>
+    <main className='w-screen h-screen bg-primary-cloud pt-[180px] overflow-x-hidden'>
+      <header className='container h-max flex-center flex-col gap-9 mx-auto'>
+        <Logo />
+        {step > 0 && (
+          <p className='roboto text-sm text-primary-sky tracking-[0.2em] uppercase'>
+            question {step} / 7
+          </p>
+        )}
+      </header>
+
+      {/* Survey steps container */}
+      <section className={`container mx-auto mt-12 pb-16 `}>
+        <SurveyStep1 />
+      </section>
+    </main>
   );
 }
 
