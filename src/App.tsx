71b13a67d259ac6c985f317cafc39e8ff85b2d8f
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-26 16:19:30 +0545

    Add wizardsteps

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,52 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+// packages
+import { useWizard } from 'use-wizard';
 
 // components
 import Logo from '@/components/Logo';
 
-import SurveyStep1 from './SurveySteps/SurveyStep1';
+// steps
+import WizardSteps from '@/SurveySteps/WizardSteps';
+
+// global
+import { SurveySteps } from '@/global/enum';
+
+// reducer
+import formReducer from '@/reducer';
 
 function App() {
-  const [step] = useState(0);
+  const [step, wizard] = useWizard([
+    SurveySteps.INTRO,
+    SurveySteps.OS,
+    SurveySteps.FRAMEWORK,
+    SurveySteps.SESSION,
+    SurveySteps.ANALYTICS,
+    SurveySteps.APP_LINK,
+    SurveySteps.IMPROVEMENT,
+    SurveySteps.EMAIL,
+    SurveySteps.END,
+  ]);
+
+  // This is only used to store user inputs
+  const [form, dispatchForm] = useReducer(formReducer, {});
 
   return (
     <main className='w-screen h-screen bg-primary-cloud pt-[180px] overflow-x-hidden'>
       <header className='container h-max flex-center flex-col gap-9 mx-auto'>
         <Logo />
-        {step > 0 && (
-          <p className='roboto text-sm text-primary-sky tracking-[0.2em] uppercase'>
-            question {step} / 7
-          </p>
-        )}
+        {wizard.stepIndex() > 0 &&
+          wizard.stepIndex() !== wizard.wizardLength() - 1 && (
+            <p className='roboto text-sm text-primary-sky tracking-[0.2em] uppercase'>
+              {/* -2 because we don't want to count the intro and end steps */}
+              question {wizard.stepIndex()} / {wizard.wizardLength() - 2}
+            </p>
+          )}
       </header>
 
       {/* Survey steps container */}
       <section className={`container mx-auto mt-12 pb-16 `}>
-        <SurveyStep1 />
+        <WizardSteps {...{ step, wizard, form, dispatchForm }} />
       </section>
     </main>
   );
