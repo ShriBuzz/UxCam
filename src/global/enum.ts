d195d6fe76b67afe374c9ce94e1a0d9cd52f7fe7
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2023-01-26 15:53:31 +0545

    Add global enum

---
+++ src/global/enum.ts
@@ -0,0 +1,11 @@
+export enum SurveySteps {
+  INTRO = 'intro',
+  OS = 'os-select',
+  FRAMEWORK = 'framework-select',
+  SESSION = 'session-select',
+  ANALYTICS = 'analytics-select',
+  APP_LINK = 'app-link',
+  IMPROVEMENT = 'improvement-select',
+  EMAIL = 'email',
+  END = 'end',
+}
